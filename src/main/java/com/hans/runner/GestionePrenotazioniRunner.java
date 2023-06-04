1735e959340f5fea83270b4816741d537422a3ec
gretel-hans <adjeihansel@icloud.com>
2023-06-05 00:49:34 +0200

    .

--- src/main/java/com/hans/runner/GestionePrenotazioniRunner.java
+++ src/main/java/com/hans/runner/GestionePrenotazioniRunner.java
@@ -22,131 +22,137 @@ import com.hans.service.UtenteService;
 @Component
 public class GestionePrenotazioniRunner implements ApplicationRunner {
 
-	@Autowired private UtenteService utenteService;
-	
-	@Autowired private EdificioService edificioService;
-	
-	@Autowired private PostazioneService postazioneService;
-	
-	@Autowired private PrenotazionePostazioneService prenotazioneService;
+	@Autowired
+	private UtenteService utenteService;
 
-	
+	@Autowired
+	private EdificioService edificioService;
+
+	@Autowired
+	private PostazioneService postazioneService;
+
+	@Autowired
+	private PrenotazionePostazioneService prenotazioneService;
+
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
-		
-		//CREAZIONE UTENTI
-		for(int i=0;i<20;i++) {
-			Utente u=utenteService.creaUtente();
-			//utenteService.salvaOModificaUtente(u);
+
+		// CREAZIONE UTENTI
+		for (int i = 0; i < 20; i++) {
+			Utente u = utenteService.creaUtente();
+			// utenteService.salvaOModificaUtente(u);
 		}
-		//CREAZIONE EDIFICI
-		for(int i=0;i<20;i++) {
-			Edificio e=edificioService.creaEdificio();
-			//edificioService.salvaOModificaEdificio(e);
+		// CREAZIONE EDIFICI
+		for (int i = 0; i < 20; i++) {
+			Edificio e = edificioService.creaEdificio();
+			// edificioService.salvaOModificaEdificio(e);
 		}
-		
-		//CREAZIONE POSTAZIONE 
-		
-		String[] listaDescrizioni= {"Postazione Minimalista: Una scrivania moderna con linee pulite e minimali"
-				,"Postazione Creativa: Una scrivania colorata e vivace con un sacco di elementi stimolanti"
-				,"Postazione Tecnologica: Una scrivania high-tech con un monitor ultrawide curvo"
-				,"Postazione Organizzata: Una scrivania impeccabilmente ordinata",
+
+		// CREAZIONE POSTAZIONE
+
+		String[] listaDescrizioni = { "Postazione Minimalista: Una scrivania moderna con linee pulite e minimali",
+				"Postazione Creativa: Una scrivania colorata e vivace con un sacco di elementi stimolanti",
+				"Postazione Tecnologica: Una scrivania high-tech con un monitor ultrawide curvo",
+				"Postazione Organizzata: Una scrivania impeccabilmente ordinata",
 				"Postazione Verde: Una scrivania circondata da piante d'appartamento",
 				"Postazione Collaborativa: Una grande scrivania rettangolare che può ospitare più persone",
 				"Postazione Accogliente: Una scrivania con una sedia ergonomica super confortevole",
 				"Postazione Moderna: Una scrivania con superficie di vetro temperato e gambe in metallo lucido",
 				"Postazione High-Tech: Una scrivania dotata di un sistema di realtà virtuale",
-				"Postazione Motivante: Una scrivania con una bacheca degli obiettivi"};
-		
-		TipoPostazione[] sceltaTipo= {TipoPostazione.Openspace,TipoPostazione.Sala_Riunioni,TipoPostazione.Privato};
-		for (int i=0;i<30;i++) {
-			long idEdificio= (long)(Math.random()*20)+1;
-			int tipoPostazione=(int)(Math.random()*3);
-			int descrizionePostazione=(int)(Math.random()*9);
-			int numeroMaxOccupanti=(int)(Math.random()*60)+10;
-			
-			Postazione p=new Postazione(listaDescrizioni[descrizionePostazione],numeroMaxOccupanti,sceltaTipo[tipoPostazione], edificioService.cercaEdificio(idEdificio));
-		//	postazioneService.salvaOModificaPostazione(p);
-		}
-		
-		
-		//RICERCA DI UNA DETERMINATA POSTAZIONE IN BASE ALLA CITTA
-		//E IL TIPO DI POSTAZIONE INSERITO
-		
-		List<Postazione> listaP=postazioneService.cercaPostazionePerTipoCitta("Milano",TipoPostazione.Privato);
-		//listaP.forEach(p->System.out.println("Postazione situata a: "+p.getEdificio().getCitta()+" [ "+p.getDescrizione()+", numero massimo persone: "+p.getNumeroMassimoOccupanti()+", tipo: "+p.getTipoPostazione()+" ]"));
-		
+				"Postazione Motivante: Una scrivania con una bacheca degli obiettivi" };
 
-	//PRENOTAZIONE POSTAZIONI
-	
-	for(int i=0;i<25;i++) {
-		long start=LocalDate.of(2020, 01, 01).toEpochDay();
-		long end=LocalDate.now().toEpochDay();
-		long random = ThreadLocalRandom
-				.current()
-				.nextLong(start, end);
-		long idUtente= (long)(Math.random()*20)+1;
-		long idPostazione= (long)(Math.random()*25)+1;
-		
-		
-		PrenotazionePostazione p=new PrenotazionePostazione(utenteService.cercaUtente(idUtente), postazioneService.cercaPostazione(idPostazione), LocalDate.ofEpochDay(random));
-		//prenotazioneService.salvaOModficaPrenotazionePostazione(p);
-	}
-    
-	//PROVA PRENOTAZIONE STESSO UTENTE STESSA DATA E POSTAZIONE DIVERSA
-	PrenotazionePostazione p2=new PrenotazionePostazione(utenteService.cercaUtente(1l), postazioneService.cercaPostazione(4l), LocalDate.of(2023,06,10));
-	//prenotazioneService.salvaOModficaPrenotazionePostazione(p2);
-	
-	
-    //CAMBIO DATA PRENOTAZIONE DELL'UTENTE SOPRA
-	PrenotazionePostazione p3=prenotazioneService.cercaPrenotazionePostazione(16l);
-	p3.setDataPrenotazione(LocalDate.of(2023, 06, 13));
-	//prenotazioneService.salvaOModficaPrenotazionePostazione(p3);
-	
-	
-	//PRENOTAZIONE CON LO STESSO UTENTE LA DATA PRIMA DELLA MODIFICA
-	//PER VEDERE SE MI PERMETTE DI PRENOTARE
-	
-	PrenotazionePostazione preno=new PrenotazionePostazione(utenteService.cercaUtente(1l), postazioneService.cercaPostazione(1l), LocalDate.of(2023,06,10));
-	//prenotazioneService.salvaOModficaPrenotazionePostazione(preno);
-	
-	
-	//STAMPA TUTTE LE PRENOTAZIONI
-	List <PrenotazionePostazione> listAllPrenotazioni=prenotazioneService.cercaTuttePrenotazioni();
-	//listAllPrenotazioni.forEach(p->System.out.println(p));
-	
-	//STAMPA TUTTI GLI UTENTI
-	List <Utente> listAllUtenti=utenteService.cercaTuttiUtenti();
-	//listAllPrenotazioni.forEach(u->System.out.println(u));
-	
-	//STAMPA TUTTE LE POSTAZIONI
-	List<Postazione> listAllPostazioni=postazioneService.cercaTuttePostazioni();
-	//listAllPostazioni.forEach(p->System.out.println(p));
-	
-	//STAMPA TUTTI GLI EDIFICI 
-	List<Edificio> listAllEdifici=edificioService.cercaTuttiEdifici();
-	//listAllPostazioni.forEach(e->System.out.println(e));
-	
-	
-	
-	/*PASSATA UNA AZIENDA E DUE DATE SI POSSONO VEDERE 
-	QUANTE POSTAZIONI DELL'EDIFICIO DELL'AZIENDA PASSATA SONO STATE PRENOTATE 
-	NELLE DATE INDICATE*/
-	//prenotazioneService.cercaNumeroPrenotazioniAziendaInData(edificioService.cercaEdificio(2l), LocalDate.of(2000, 01, 01), LocalDate.now());
-	
-	
-	/*PASSATA UNA AZIENDA RITORNA QUANTE POSTAZIONI L'AZIENDA 
-	HA NEL DATABASE */
-	//postazioneService.cercaPostazioneAzienda(edificioService.cercaEdificio(2l));
-	
-	
-	
-	
-	}
-	
-		
-		}
-	
-	
-	
+		TipoPostazione[] sceltaTipo = { TipoPostazione.Openspace, TipoPostazione.Sala_Riunioni,
+				TipoPostazione.Privato };
+		for (int i = 0; i < 30; i++) {
+			long idEdificio = (long) (Math.random() * 20) + 1;
+			int tipoPostazione = (int) (Math.random() * 3);
+			int descrizionePostazione = (int) (Math.random() * 9);
+			int numeroMaxOccupanti = (int) (Math.random() * 60) + 10;
 
+			Postazione p = new Postazione(listaDescrizioni[descrizionePostazione], numeroMaxOccupanti,
+					sceltaTipo[tipoPostazione], edificioService.cercaEdificio(idEdificio));
+			// postazioneService.salvaOModificaPostazione(p);
+		}
+
+		// RICERCA DI UNA DETERMINATA POSTAZIONE IN BASE ALLA CITTA
+		// E IL TIPO DI POSTAZIONE INSERITO
+
+		List<Postazione> listaP = postazioneService.cercaPostazionePerTipoCitta("Milano", TipoPostazione.Privato);
+		// listaP.forEach(p->System.out.println("Postazione situata a:
+		// "+p.getEdificio().getCitta()+" [ "+p.getDescrizione()+", numero massimo
+		// persone: "+p.getNumeroMassimoOccupanti()+", tipo: "+p.getTipoPostazione()+"
+		// ]"));
+
+		// PRENOTAZIONE POSTAZIONI
+
+		for (int i = 0; i < 25; i++) {
+			long start = LocalDate.of(2020, 01, 01).toEpochDay();
+			long end = LocalDate.now().toEpochDay();
+			long random = ThreadLocalRandom
+					.current()
+					.nextLong(start, end);
+			long idUtente = (long) (Math.random() * 20) + 1;
+			long idPostazione = (long) (Math.random() * 25) + 1;
+
+			PrenotazionePostazione p = new PrenotazionePostazione(utenteService.cercaUtente(idUtente),
+					postazioneService.cercaPostazione(idPostazione), LocalDate.ofEpochDay(random));
+			// prenotazioneService.salvaOModficaPrenotazionePostazione(p);
+		}
+
+		// PROVA PRENOTAZIONE STESSO UTENTE STESSA DATA E POSTAZIONE DIVERSA
+		PrenotazionePostazione p2 = new PrenotazionePostazione(utenteService.cercaUtente(1l),
+				postazioneService.cercaPostazione(4l), LocalDate.of(2023, 06, 10));
+		// prenotazioneService.salvaOModficaPrenotazionePostazione(p2);
+
+		// PRENOTAZIONE CON LO STESSO UTENTE LA DATA PRIMA DELLA MODIFICA
+		// PER VEDERE SE MI PERMETTE DI PRENOTARE
+		PrenotazionePostazione preno = new PrenotazionePostazione(utenteService.cercaUtente(1l),
+				postazioneService.cercaPostazione(1l), LocalDate.of(2023, 06, 10));
+		// prenotazioneService.salvaOModficaPrenotazionePostazione(preno);
+
+		// CAMBIO DATA PRENOTAZIONE DELL'UTENTE SOPRA
+		PrenotazionePostazione p3 = prenotazioneService.cercaPrenotazionePostazione(16l);
+		p3.setDataPrenotazione(LocalDate.of(2023, 06, 13));
+		// prenotazioneService.salvaOModficaPrenotazionePostazione(p3);
+
+		// PRENOTAZIONE CON LO STESSO UTENTE LA DATA DOPO LA MODIFICA
+		// PER VEDERE SE MI PERMETTE DI PRENOTARE
+		PrenotazionePostazione preno2 = new PrenotazionePostazione(utenteService.cercaUtente(1l),
+				postazioneService.cercaPostazione(1l), LocalDate.of(2023, 06, 10));
+		// prenotazioneService.salvaOModficaPrenotazionePostazione(preno2);
+
+
+		
+		// STAMPA TUTTE LE PRENOTAZIONI
+		List<PrenotazionePostazione> listAllPrenotazioni = prenotazioneService.cercaTuttePrenotazioni();
+		// listAllPrenotazioni.forEach(p->System.out.println(p));
+
+		// STAMPA TUTTI GLI UTENTI
+		List<Utente> listAllUtenti = utenteService.cercaTuttiUtenti();
+		// listAllPrenotazioni.forEach(u->System.out.println(u));
+
+		// STAMPA TUTTE LE POSTAZIONI
+		List<Postazione> listAllPostazioni = postazioneService.cercaTuttePostazioni();
+		// listAllPostazioni.forEach(p->System.out.println(p));
+
+		// STAMPA TUTTI GLI EDIFICI
+		List<Edificio> listAllEdifici = edificioService.cercaTuttiEdifici();
+		// listAllPostazioni.forEach(e->System.out.println(e));
+
+		/*
+		 * PASSATA UNA AZIENDA E DUE DATE SI POSSONO VEDERE
+		 * QUANTE POSTAZIONI DELL'EDIFICIO DELL'AZIENDA PASSATA SONO STATE PRENOTATE
+		 * NELLE DATE INDICATE
+		 */
+		// prenotazioneService.cercaNumeroPrenotazioniAziendaInData(edificioService.cercaEdificio(2l),
+		// LocalDate.of(2000, 01, 01), LocalDate.now());
+
+		/*
+		 * PASSATA UNA AZIENDA RITORNA QUANTE POSTAZIONI L'AZIENDA
+		 * HA NEL DATABASE
+		 */
+		// postazioneService.cercaPostazioneAzienda(edificioService.cercaEdificio(2l));
+
+	}
+
+}
